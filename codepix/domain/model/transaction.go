b5a2723aaa9ea3acbccf792542093b976755d0de
wslmacieira <wslmacieira@outlook.com>
2022-04-08 23:25:52 -0300

    mapeando colunas

--- codepix/domain/model/transaction.go
+++ codepix/domain/model/transaction.go
@@ -28,13 +28,13 @@ type Transactions struct {
 type Transaction struct {
 	Base              `valid:"required"`
 	AccountFrom       *Account `valid:"-"`
-	AccountFromID     string   `json:"account_from_id" valid:"notnull"`
-	Amount            float64  `json:"amount" valid:"notnull"`
+	AccountFromID     string   `gorm:"column:account_from_id;type:uuid;" valid:"notnull"`
+	Amount            float64  `json:"amount" gorm:"type:float" valid:"notnull"`
 	PixKeyTo          *PixKey  `valid:"-"`
-	PixKeyIdTo        string   `json:"pix_key_id_to" valid:"notnull"`
-	Status            string   `json:"status" valid:"notnull"`
-	Description       string   `json:"description" valid:"-"`
-	CancelDescription string   `json:"cancel_description" valid:"-"`
+	PixKeyIdTo        string   `gorm:"column:pix_key_id_to;type:uuid;" valid:"notnull"`
+	Status            string   `json:"status" gorm:"type:varchar(20)" valid:"notnull"`
+	Description       string   `json:"description" gorm:"type:varchar(255)" valid:"-"`
+	CancelDescription string   `json:"cancel_description" gorm:"type:varchar(255)" valid:"-"`
 }
 
 func (t *Transaction) isValid() error {
