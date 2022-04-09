32eeb26c535052c389ec85a96622124ff9cdfae7
wslmacieira <wslmacieira@outlook.com>
2022-04-08 21:01:08 -0300

    criando bank model

---
+++ codepix/domain/model/bank.go
@@ -0,0 +1,36 @@
+package model
+
+import (
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+type Bank struct {
+	ID        string    `json:"id"`
+	Code      string    `json:"code"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func (bank *Bank) isValid() error {
+	return nil
+}
+
+func NewBank(code string, name string) (*Bank, error) {
+	bank := Bank{
+		Code: code,
+		Name: name,
+	}
+
+	bank.ID = uuid.NewV4().String()
+	bank.CreatedAt = time.Now()
+
+	err := bank.isValid()
+	if err != nil {
+		return nil, err
+	}
+
+	return &bank, nil
+}
