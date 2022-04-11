9dca0a5c0f7638f7827edff006b396eb04190ab3
wslmacieira <wslmacieira@outlook.com>
2022-04-11 01:44:08 -0300

    criando transaction controller

--- bank-api/src/app.module.ts
+++ bank-api/src/app.module.ts
@@ -11,6 +11,8 @@ import { PixKeyController } from './controllers/pix-key/pix-key.controller';
 import { PixKey } from './models/pix-key.model';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { join } from 'path';
+import { TransactionController } from './controllers/transaction/transaction.controller';
+import { Transaction } from './models/transaction.model';
 
 @Module({
   imports: [
@@ -23,9 +25,9 @@ import { join } from 'path';
       username: process.env.TYPEORM_USERNAME,
       password: process.env.TYPEORM_PASSWORD,
       database: process.env.TYPEORM_DATABASE,
-      entities: [BankAccount, PixKey],
+      entities: [BankAccount, PixKey, Transaction],
     }),
-    TypeOrmModule.forFeature([BankAccount, PixKey]),
+    TypeOrmModule.forFeature([BankAccount, PixKey, Transaction]),
     ClientsModule.register([
       {
         name: 'CODEPIX_PACKAGE',
@@ -37,8 +39,32 @@ import { join } from 'path';
         },
       },
     ]),
+    ClientsModule.register([
+      {
+        name: 'TRANSACTION_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: process.env.KAFKA_CLIENT_ID,
+            brokers: [process.env.KAFKA_BROKER],
+          },
+          consumer: {
+            groupId:
+              !process.env.KAFKA_CONSUMER_GROUP_ID ||
+              process.env.KAFKA_CONSUMER_GROUP_ID === ''
+                ? 'my-consumer-' + Math.random()
+                : process.env.KAFKA_CONSUMER_GROUP_ID,
+          },
+        },
+      },
+    ]),
   ],
-  controllers: [AppController, BankAccountController, PixKeyController],
+  controllers: [
+    AppController,
+    BankAccountController,
+    PixKeyController,
+    TransactionController,
+  ],
   providers: [AppService, FixturesCommand],
 })
 export class AppModule {}
